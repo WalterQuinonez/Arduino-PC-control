116a1e83209896610926f91d1792228036614605
WalterQuinonez <147729543+WalterQuinonez@users.noreply.github.com>
2025-06-24 13:51:09 -0300

    Add files via upload

--- control_fuente_test_tiempo_comunicacion.py
+++ control_fuente_test_tiempo_comunicacion.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jun 11 13:30:03 2025
+Created on Tue Jun 24 13:34:22 2025
 
 @author: walte
 """
+
+
 import serial
 import time
+
 
 
 def enviar_comando(comando):
@@ -16,27 +19,6 @@ def enviar_comando(comando):
     return respuesta
 
 
-def aplicar_voltaje(v,tapp, toff ):
-    
-    current_read = []
-    vout = 3
-    enviar_comando(f'PV {vout}')
-    #loop para que el equipo me mande mediciones mientras completo el tiempo de aplicacion
-    #dada la velocidad de comunicación, pedir data en intervalos menores a 1 seg es al pedo
-    for i in range(5):
-        current_read.append(str(enviar_comando('DVC?')))
-        #time.sleep(tapp) #esto no se si es necesario, probrar primero sin usar esto
-                          #el tema es que quizas este comando ya mete un segundo de delay
-    #Repito para el tiempo muerto
-    vout = 0
-    enviar_comando(f'PV {vout}')
-    current_read.append(str(enviar_comando('DVC?')))
-    for i in range(5):
-        current_read.append(str(enviar_comando('DVC?')))
-
-    return current_read
-    
-    
 
 # Configuración del puerto serial
 puerto = 'COM4' # Cambia &#39;COM3&#39; por el puerto serial que uses (ej.&#39;/dev/ttyUSB0&#39; en Linux)
@@ -57,5 +39,32 @@ enviar_comando('CLS')
 enviar_comando('OUT 1') #prende y apaga la salida
    
 
-voltaje = enviar_comando('PC 6') # con esto fijo la corriente de salida maxima
-current_read = aplicar_voltaje(v,tapp, toff )    
+
+
+'''PROBAR ESTO PRIMERO Y CHEQUEAR LOS TIEMPO DE LA COMUNICACION'''
+
+vout = 3
+tiempo = time.time()
+enviar_comando(f'PV {vout}')
+print(f'tiempo de comunicacion {time.time() - tiempo } segundos')
+#loop para que el equipo me mande mediciones mientras completo el tiempo de aplicacion
+#dada la velocidad de comunicación, pedir data en intervalos menores a 1 seg es al pedo
+for i in range(5):
+    tiempo = time.time()
+    current_read.append(str(enviar_comando('DVC?')))
+    print(f'tiempo de comunicacion {time.time() - tiempo } segundos')
+
+    #time.sleep(tapp) #esto no se si es necesario, probrar primero sin usar esto
+                      #el tema es que quizas este comando ya mete un segundo de delay
+#Repito para el tiempo muerto
+vout = 0
+tiempo = time.time()
+enviar_comando(f'PV {vout}')
+print(f'tiempo de comunicacion {time.time() - tiempo } segundos')
+
+for i in range(5):
+    tiempo = time.time()
+    current_read.append(str(enviar_comando('DVC?')))
+    print(f'tiempo de comunicacion {time.time() - tiempo } segundos')
+
+
